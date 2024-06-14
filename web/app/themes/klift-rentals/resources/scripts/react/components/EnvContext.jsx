e393ddf5ed9e4aa19bd274716bb2e3eb034496e4
Narinder <narinder@Bizdesires-MacBook-Air-2.local>
2024-06-14 13:30:13 +0530

    first

--- web/app/themes/klift-rentals/resources/scripts/react/components/EnvContext.jsx
+++ web/app/themes/klift-rentals/resources/scripts/react/components/EnvContext.jsx
@@ -1,13 +1,14 @@
-
-// import { useEffect, useState } from 'react';
-
-// const EnvProvider = () => {
-   
-//   return {
-//     baseUrl : 'https://klift-rentals.test/wp-json/sage-endpoint/v1/',
-//   };
+// export default  {
+//     baseUrl : 'https://klifts.test/wp-json/sage-endpoint/v1/',
+//     url : 'https://klifts.test/',
+//     wcUrl : 'https://klifts.test/wp-json/',
+//     ajaxUrl:'https://klifts.test/wp/wp-admin/admin-ajax.php'
 // };
 
+
 export default  {
-    baseUrl : 'https://klifts.test/wp-json/sage-endpoint/v1/',
+    baseUrl : 'http://18.222.127.15/forklift-rental/web/wp-json/sage-endpoint/v1/',
+    url : 'http://18.222.127.15/forklift-rental/web/',
+    wcUrl : 'http://18.222.127.15/forklift-rental/web/wp-json/',
+    ajaxUrl:'http://18.222.127.15/forklift-rental/web/wp/wp-admin/admin-ajax.php'
 };
