3957a03578216a7aae834a3bbbaf62f5d3660cfc
Narinder <narinder@Bizdesires-MacBook-Air-2.local>
2024-06-18 14:43:05 +0530

    test

--- web/app/themes/klift-rentals/resources/scripts/react/components/EnvContext.jsx
+++ web/app/themes/klift-rentals/resources/scripts/react/components/EnvContext.jsx
@@ -1,14 +1,14 @@
-export default  {
-    baseUrl : 'https://klifts.test/wp-json/sage-endpoint/v1/',
-    url : 'https://klifts.test/',
-    wcUrl : 'https://klifts.test/wp-json/',
-    ajaxUrl:'https://klifts.test/wp/wp-admin/admin-ajax.php'
-};
-
-
 // export default  {
-//     baseUrl : 'http://18.222.127.15/wp-json/sage-endpoint/v1/',
-//     url : 'http://18.222.127.15/',
-//     wcUrl : 'http://18.222.127.15/wp-json/',
-//     ajaxUrl:'http://18.222.127.15/wp/wp-admin/admin-ajax.php'
+//     baseUrl : 'https://klifts.test/wp-json/sage-endpoint/v1/',
+//     url : 'https://klifts.test/',
+//     wcUrl : 'https://klifts.test/wp-json/',
+//     ajaxUrl:'https://klifts.test/wp/wp-admin/admin-ajax.php'
 // };
+
+
+export default  {
+    baseUrl : 'http://18.222.127.15/wp-json/sage-endpoint/v1/',
+    url : 'http://18.222.127.15/',
+    wcUrl : 'http://18.222.127.15/wp-json/',
+    ajaxUrl:'http://18.222.127.15/wp/wp-admin/admin-ajax.php'
+};
